62036eda22d4da40fa7c3ac7f2fa46019d8e47f4
Lucas Borges <lucas.borges@portalsolar.com.br>
2023-06-19 16:08:18 -0300

    feat: Componentes da tela Home Criados e OAuth do Github com response do code criado

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        href="https://github.com/lucasborgesdasilva"
+        target="_blank"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        mim
+      </a>{' '}
+      no NLW da 🚀{' '}
+      <a
+        href="https://www.rocketseat.com.br/"
+        target="_blank"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
